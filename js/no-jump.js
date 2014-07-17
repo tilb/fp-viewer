da7f8801b39cfda0dd72896277d4f32904cbc6c3
Ernest Mistiaen <mistiaen@gmail.com>
2014-07-17 10:28:49 +0200

    Backup commit

--- js/no-jump.js
+++ js/no-jump.js
@@ -1,1 +1,12 @@
+console.log("before handler")
 
+$("a[href^=#]").on("click", function(e) {
+  e.preventDefault();
+  console.log("Hallo")
+  history.pushState({}, "", this.href);
+});
+
+iets = document.getElementById("iets1")
+iets.onclick = new function() {
+  #console.log("in iets1")
+}
